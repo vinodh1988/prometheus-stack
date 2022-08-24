71f540f3ae6d801c0d57e13265b56a06833bd5fc
vinodh1988 <k.c.vinodh@hotmail.com>
2022-08-24 09:03:14 +0530

    other-types

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import http.server
-from prometheus_client import Counter,start_http_server
+import random
+from prometheus_client import Counter,start_http_server,Gauge,Histogram
 
 no_of_requests = Counter('app_requests_so_far','no of requests made so far',['app_name','endpoint'])
-
+current_active_users = Gauge('current_active_users','Current active users')
+number_of_workers=Histogram('number_of_workers','Number of worker threads to process the request', buckets=[0,10,20,30,40,50,60,70,80,90,100])
 
 PORT = 8000
 METRICS_PORT=8001
@@ -10,6 +12,8 @@ METRICS_PORT=8001
 class HandleRequests(http.server.http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         no_of_requests.labels('myapi',self.path).inc()
+        current_active_users.set(random.random()*10000)
+        number_of_workers.observe(random.random()*100)
         self.send_response(200)
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
